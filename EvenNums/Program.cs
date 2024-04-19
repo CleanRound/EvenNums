e5ca4c32193342feec9bea48acc30ca190b24ec5
Олег Луцик <oleglutsik.dev@gmail.com>
2024-04-19 19:56:43 +0300

    Добавьте файлы проекта.

---
+++ EvenNums/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the range to find all even numbers:");
+        if (int.TryParse(Console.ReadLine(), out int num1) && int.TryParse(Console.ReadLine(), out int num2))
+        {
+            int start, end;
+            if (num1 <= num2)
+            {
+                start = num1;
+                end = num2;
+            }
+            else
+            {
+                start = num2;
+                end = num1;
+            }
+
+            Console.WriteLine($"Even numbers in the range [{start}, {end}]:");
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error: Invalid input.");
+        }
+    }
+}
